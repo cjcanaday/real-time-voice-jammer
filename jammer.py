283bbc1581debcc71898cf404caacef941bfbb4c
Colin Canaday <cjcanaday@gmail.com>
2025-04-14 15:55:11 -0700

    Working proof of concept

--- jammer.py
+++ jammer.py
@@ -3,26 +3,29 @@ import numpy as np
 from collections import deque
 
 # config values
-DELAY = 0.3 # seconds
+DELAY = 0.15 # seconds
 SAMPLE_RATE = 44100
 BLOCK_SIZE = 1024
 
-DELAY_SAMPLES = int(DELAY * SAMPLE_RATE)
+DELAY_SAMPLES = int(np.ceil(DELAY * SAMPLE_RATE / BLOCK_SIZE))
 BUFFER = deque(maxlen=DELAY_SAMPLES)
 
 def jammer(indata, outdata, frames, time, status):
-    if(status):
-        print(status)
+    if status:
+        print(status, flush=True)
 
-    for i in range(len(indata)):
-        BUFFER.append(indata[i].copy())
-        if(len(BUFFER) == BUFFER.maxlen):
-            outdata[i] = BUFFER[0] # if buffer is full, return old audio from DELAY seconds ago
-        else:
-            outdata[i] = np.zeros_like(indata[i]) # if buffer isn't full, return no sound
+    # Store a copy of the current input block
+    BUFFER.append(indata.copy())
+
+    if len(BUFFER) == BUFFER.maxlen:
+       # if buffer is full, return old audio from DELAY seconds ago
+        outdata[:] = BUFFER[0]
+    else:
+        # if buffer isn't full, return no sound
+        outdata[:] = np.zeros_like(indata)
     
 try:
-    with sd.Stream(channels=1, callback=jammer, samplerate=SAMPLE_RATE, blocksize=BLOCK_SIZE):
+    with sd.Stream(device=(1,3),channels=1, dtype='float32', callback=jammer, samplerate=SAMPLE_RATE, blocksize=BLOCK_SIZE):
         while True:
             pass  # Keep the stream open
 except KeyboardInterrupt:
